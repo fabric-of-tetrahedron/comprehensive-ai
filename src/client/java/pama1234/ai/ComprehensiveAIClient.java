d9b8ce790766fe25d4397aecc676bb621c5990c7
pama1234 <pama1234@163.com>
2024-05-08 13:44:29 +0800

    init

---
+++ src/client/java/pama1234/ai/ComprehensiveAIClient.java
@@ -0,0 +1,10 @@
+package pama1234.ai;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ComprehensiveAIClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
